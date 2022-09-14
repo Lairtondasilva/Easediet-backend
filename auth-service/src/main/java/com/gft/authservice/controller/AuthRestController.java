9902ce1524aacb5569684f2343e006dfbf29206e
Ferreira <toer@gft.com>
2022-09-14 16:48:53 -0300

    FEAT: Added Auth-Service layer

---
+++ auth-service/src/main/java/com/gft/authservice/controller/AuthRestController.java
@@ -0,0 +1,32 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gft.authservice.service.util.JwtUtil;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthRestController {
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody String userName) {
+		String token = jwtUtil.generateToken(userName);
+
+		return new ResponseEntity<String>(token, HttpStatus.OK);
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<String> register(@RequestBody String userName) {
+
+		System.out.println("Information Saved!");
+
+		return new ResponseEntity<String>("Registered", HttpStatus.OK);
+	}
+}
