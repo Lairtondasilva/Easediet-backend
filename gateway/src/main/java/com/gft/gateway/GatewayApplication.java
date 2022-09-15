2439b524b9456bb9d00a95bffc8e51c4804050d3
da Silva <LNDV@gft.com>
2022-09-15 13:46:44 -0300

    FEAT: Adding ip Address property

--- gateway/src/main/java/com/gft/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gft/gateway/GatewayApplication.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
+
 @SpringBootApplication
 @EnableFeignClients
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
 
-	// @Bean
-	// @LoadBalanced
-	// public WebClient.Builder loadBalancedWebClientBuilder() {
-	// return WebClient.builder();
-	// }
+		SpringApplication.run(GatewayApplication.class, args);
+
+	}
 }
