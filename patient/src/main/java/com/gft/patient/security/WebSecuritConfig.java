9088201c06f13e0c27e2875a0181626b1e70a340
da Silva <LNDV@gft.com>
2022-09-16 10:11:35 -0300

    FEAT: start authorization in patient

---
+++ patient/src/main/java/com/gft/patient/security/WebSecuritConfig.java
@@ -0,0 +1,22 @@
+package com.gft.patient.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecuritConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
